f4f0883b680dba0c3eb5a8af60d7d139413d24fd
sabexzero <sabexqe@yandex.ru>
2024-03-24 20:39:53 +0300

    Test actions

--- src/main/java/com/example/rockpaperscissorsultimate/config/WebSocketConfig.java
+++ src/main/java/com/example/rockpaperscissorsultimate/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.rockpaperscissorsultimate.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Override
     public void configureMessageBroker(
             MessageBrokerRegistry config
@@ -21,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(
             StompEndpointRegistry registry
     ) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins(frontendUrl).withSockJS();
     }
     
     
