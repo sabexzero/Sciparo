47485251a69c08604dfe9e14dec979fc9ba195cc
sabexzero <sabexqe@yandex.ru>
2024-03-24 21:14:58 +0300

    test -TODO_TO_ISSUE-

--- src/main/java/com/example/rockpaperscissorsultimate/domain/exceptions/general/CustomException.java
+++ src/main/java/com/example/rockpaperscissorsultimate/domain/exceptions/general/CustomException.java
@@ -1,6 +1,6 @@
 package com.example.rockpaperscissorsultimate.domain.exceptions.general;
 
-
+//TODO: Remake exception handler
 public class CustomException extends RuntimeException{
     public CustomException(
             String message
